30f44214ffd497e4effe6a4fdac4608ae6dbf89b
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-12 19:35:52 -0300

    Feat: Requisito 07 finalizado

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+
+    for process in instance.items:
+        occurrences = []
+
+        for line, content in enumerate(process['linhas_do_arquivo']):
+            if word.lower() in content.lower():
+                occurrences.append({"linha": line + 1})
+
+        if occurrences:
+            response.append({
+                "palavra": word,
+                "arquivo": process['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return response
 
 
 def search_by_word(word, instance):
