533c9c25fe260c19c3f85fbf66e2527ba8c0ca5c
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-01-12 16:34:11 -0300

    Feat: Requisito 03 finalizado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+    file_metadata = {'nome_do_arquivo': path_file,
+                     'qtd_linhas': len(file_content),
+                     'linhas_do_arquivo': file_content}
+
+    if any(file['nome_do_arquivo'] == path_file for file in instance.items):
+        return None
+
+    instance.enqueue(file_metadata)
+    print(instance.items)
+    return None
 
 
 def remove(instance):
